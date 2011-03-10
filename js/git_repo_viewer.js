6ffc486399f6bf7b249b5482ecf6dca89a7a594c
Howard Tyson <howard@howardtyson.com>
2011-03-09 20:10:17 -0500

    fixed javascript for branch switcher, began writing line selector

--- js/git_repo_viewer.js
+++ js/git_repo_viewer.js
@@ -1,7 +1,9 @@
+// TODO: Make sure we're not loading and running unnecessary code because all 
+// of our js is in this file.
 Drupal.behaviors.gitRepoViewer = function(context) {
   $('.repo-branches-form select#edit-branches').change(
     function(context) {
-      var url = Drupal.gitRepoViewer.getBasePath() + '?branch=' + $(this).val();
+      var url = location.protocol + '//' + location.hostname + location.pathname + '?branch=' + $(this).val();
       var path = '', commit = '';
       if (path = Drupal.gitRepoViewer.getQueryStrings('path')) {
         url = url + '&path=' + path;
@@ -14,14 +16,32 @@ Drupal.behaviors.gitRepoViewer = function(context) {
   );
 }
 
+Drupal.behaviors.gitRepoViewerCodeSelector = function(context) {
+  $('.git-repo-browser-content .geshifilter ol').each(
+    function () {
+      var lineNumber = 1;
+      $('li', this).each(
+        function() {
+          $(this).click(Drupal.gitRepoViewer.lineNumberClick);
+          // TODO: Lets
+          /*
+          $(this).mousedown(Drupal.gitRepoViewer.lineNumberClick);
+          $(this).mouseup(Drupal.gitRepoViewer.lineNumberClick);
+          */
+          $(this).data('gitRepoViewerLineNumber', lineNumber);
+          lineNumber++;
+        }
+      );
+    }
+  );
+}
+
 Drupal.gitRepoViewer = function() {
 }
 
-Drupal.gitRepoViewer.getBasePath = function() {
-  var regex = "(.*)\\?";
-  var regex = new RegExp(regex);
-  var results = regex.exec(window.location.href);
-  return results[1];
+Drupal.gitRepoViewer.lineNumberClick = function() {
+  var clickedLineNumber = $(this).data('gitRepoViewerLineNumber');
+  console.log(clickedLineNumber);
 }
 
 Drupal.gitRepoViewer.getQueryStrings = function(name) {
